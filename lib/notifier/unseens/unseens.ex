7f45d0fbf40a1eed5e364fd2dd449bbdc8ace46b
maabed <veto44@gmail.com>
2019-07-15 08:13:39 +0300

    refactor unseen logic: upserts using insert_all/3

--- lib/notifier/unseens/unseens.ex
+++ lib/notifier/unseens/unseens.ex
@@ -7,16 +7,6 @@ defmodule Notifier.Unseens do
   alias Notifier.{Unseen}
   alias Notifier.Repo
 
-  def get(params) do
-    case Repo.get_by(Unseen,
-          user_id: Map.get(params, :user_id, ""),
-          tribe_id: Map.get(params, :tribe_id, "")
-         ) do
-      nil -> {:error, :not_found}
-      unseen -> {:ok, unseen}
-    end
-  end
-
   def get_unseen(%{user_id: user_id, tribe_ids: tribe_ids}) do
     query = from u in Unseen,
       where: u.tribe_id in ^tribe_ids and u.user_id == ^user_id
@@ -28,36 +18,28 @@ defmodule Notifier.Unseens do
     receivers
     |> Enum.map(&String.trim/1)
     |> Enum.reject(& &1 == "")
-    |> Enum.map(fn r -> find_insert_unseen(r, tribe_id) end)
-    |> update_all_unseen(receivers, tribe_id)
+    |> Enum.uniq()
+    |> upsert_unseens(tribe_id)
   end
 
-  defp find_insert_unseen(r, tribe_id) do
-    attrs = %{tribe_id: tribe_id, user_id: r}
-    attrs
-    |> get()
-    |> build_unseen(attrs)
-  end
+  defp upsert_unseens(receiver_ids, tribe_id) do
+    now = DateTime.utc_now()
+    entries =
+      receiver_ids
+      |> Enum.map(fn id ->
+        %{
+          user_id: id,
+          tribe_id: tribe_id
+        }
+      end)
 
-  defp build_unseen({:ok, _unseen}, _), do: {:ok, nil}
-  defp build_unseen({:error, :not_found}, attrs) do
-    %Unseen{}
-    |> Unseen.changeset(attrs)
-    |> Repo.insert()
-  end
+    opts = [returning: true,
+      on_conflict: [inc: [unseen_count: 1], set: [updated_at: now, inserted_at: now]],
+      conflict_target: [:user_id, :tribe_id]]
 
-  def update_all_unseen(_, receivers, tribe_id) do
-    now = NaiveDateTime.utc_now()
     result =
-      from(u in Unseen,
-      where: u.user_id in ^receivers and u.tribe_id == ^tribe_id,
-      update: [inc: [unseen_count: ^1], set: [updated_at: ^now]],
-      select: %{
-        user_id: u.user_id,
-        tribe_id: u.tribe_id,
-        unseen_count: u.unseen_count,
-      })
-    |> Repo.update_all([])
+      Unseen
+      |> Repo.insert_all(entries, opts)
 
     case result do
       {0, error} -> {:error, error}
