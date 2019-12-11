75fd5fd590fa7c368d08769f2a169bfdf7b3a466
maabed <veto44@gmail.com>
2019-12-11 20:51:56 +0200

    fake seeds for dev

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,35 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     SapienNotifier.Repo.insert!(%SapienNotifier.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+import Ecto.Query, warn: false
+require Logger
+alias SapienNotifier.Notifier.{Notification, Receiver}
+alias SapienNotifier.Repo
+
+Enum.each(0..300, fn _ ->
+  {:ok, notification} =
+    %Notification{}
+    |> Notification.changeset(%{
+      sender_id: Faker.Address.geohash,
+      sender_name: Faker.Internet.user_name,
+      sender_thumb: Faker.Avatar.image_url,
+      sender_profile_id: Faker.Address.geohash,
+      payload: %{
+        url: Faker.Internet.url,
+        body: Faker.Name.title,
+        title: Faker.Name.title,
+        action: Faker.Name.last_name,
+        action_id: Faker.Address.geohash,
+        vote_type: "what ever"
+      }
+    })
+    |> Repo.insert()
+
+    Enum.each(0..5, fn _n ->
+      %Receiver{}
+      |> Receiver.changeset(%{
+          notification_id: notification.id,
+          user_id: Faker.Address.geohash,
+          read: false,
+          status: "UNREAD"
+      })
+      |> Repo.insert()
+  end)
+end)
