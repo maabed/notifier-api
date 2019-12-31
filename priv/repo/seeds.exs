cd3e08c738a6af6c5cf0eaf75b1a86787c9a689d
maabed <veto44@gmail.com>
2019-12-31 10:06:20 +0200

    improve seeding script

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,68 +1,46 @@
-import Ecto.Query, warn: false
-require Logger
-alias SapienNotifier.Notifier.{Notification, Receiver}
-alias SapienNotifier.Repo
 
-names = [
-  "Sabbir_Ahmed_Siddiquee",
-  "Cristian_Dascalu",
-  "Roman_Butusov",
-  "Marco_Tabasco",
-  "Katya_de_la_Rosa",
-  "Ethaanpump",
-  "Hasan_Tekin",
-  "Mahmoud_Abed",
-  "Lynn_Connolly",
-  "Robert_Giometti",
-  "Ankit_Bhatia",
-  "Javier_Ocanas"
-]
+defmodule SapienNotifier.Seed do
+  @moduledoc false
 
-actions = [
-  "mention",
-  "reply",
-  "post",
-  "comment",
-  "echo",
-  "selected",
-  "moderatorInvite",
-  "tribeInvite",
-  "follow",
-  "requested"
-]
+  import Ecto.Query, warn: false
+  require Logger
+  alias SapienNotifier.Notifier.{Notification, Receiver}
+  alias SapienNotifier.Repo
 
-Enum.each(0..1_000, fn _ ->
-  alphabet = Enum.to_list(?a..?z) ++ Enum.to_list(?0..?9)
-  sender_name = Enum.random(names)
-  action = Enum.random(actions)
-  action_id = alphabet |> Enum.take_random(33) |> to_string
+  defp random_hex_bytes(num \\ 8) do
+    :crypto.strong_rand_bytes(num) |> Base.encode16
+  end
 
-  {:ok, notification} =
-    %Notification{}
-    |> Notification.changeset(%{
-      sender_id: alphabet |> Enum.take_random(33) |> to_string,
-      sender_name: sender_name,
-      sender_thumb: "http://sapien.network/@#{sender_name}",
-      sender_profile_id: alphabet |> Enum.take_random(33) |> to_string,
-      payload: %{
-        url: "http://sapien.network/@#{sender_name}/#{action_id}",
-        body: action,
-        title: action,
-        action: action,
-        action_id: action_id,
-        vote_type: "up"
-      }
-    })
-    |> Repo.insert(log: false)
+  def run do
+    Enum.each(0..200_000, fn _ ->
+      random_id = random_hex_bytes(12)
 
-    Enum.each(0..5, fn _n ->
-      %Receiver{}
-      |> Receiver.changeset(%{
-          notification_id: notification.id,
-          user_id: alphabet |> Enum.take_random(33) |> to_string,
-          read: false,
-          status: "UNREAD"
-      })
-      |> Repo.insert(log: false)
-  end)
-end)
+      {:ok, notification} =
+        Repo.insert(%Notification{
+          sender_id: random_id,
+          sender_name: "tester",
+          sender_thumb: "http://sapien.network/@tester",
+          sender_profile_id: random_id,
+          payload: %{
+            url: "http://sapien.network/@tester/#{random_id}",
+            body: "post",
+            title: "post",
+            action: "post",
+            action_id: random_id,
+            vote_type: "up"
+          }
+        }, log: false)
+
+        Enum.each(0..5, fn _n ->
+          Repo.insert(%Receiver{
+            notification_id: notification.id,
+            user_id: random_id,
+            read: false,
+            status: "UNREAD"
+          }, log: false)
+      end)
+    end)
+  end
+end
+
+SapienNotifier.Seed.run()
